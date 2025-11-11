e4c57965254b1dbd07d4de12121eee242e748989
Heri Fauzan <heri.fauzan@ecomindo.com>
2025-11-12 06:52:53 +0700

    update compose

--- init-db.sql
+++ init-db.sql
@@ -1,7 +1,11 @@
-CREATE TABLE IF NOT EXISTS products (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    price DECIMAL(10, 2) NOT NULL,
-    stock INT DEFAULT 0
+CREATE TABLE IF NOT EXISTS "Products" (
+    "Id" SERIAL PRIMARY KEY,
+    "Name" VARCHAR(255) NOT NULL,
+    "Description" TEXT,
+    "Price" DECIMAL(10, 2) NOT NULL,
+    "Stock" INT DEFAULT 0
 );
+INSERT INTO "Products" ("Name", "Description", "Price", "Stock") VALUES
+('Product A', 'Description for Product A', 19.99, 100),
+('Product B', 'Description for Product B', 29.99, 150),
+('Product C', 'Description for Product C', 39.99, 200);
